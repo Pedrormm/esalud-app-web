a44cd9a9d7f469bdd73cff2fcabf8cb3afd27c73
Pedrormm <34313373+Pedrormm@users.noreply.github.com>
2020-02-24 09:09:50 +0100

    token saved in remember token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\PieceNew;
+use App\Mail\EnviarMail;
+use Mail;
+
 use Illuminate\Support\Str;
 
 class LoginController extends Controller
@@ -45,8 +48,9 @@ class LoginController extends Controller
         
         $maxSteps = 1000; //Por seguridad, no vamos a permitir esto
         $try = 0;
-        do {
+        do {            
             $token = Str::random(32);
+            var_dump($token);
             $user->remember_token = $token;
             if($try >= $maxSteps) {
                 // Caso muy excepcional, que no deberia pasar a no ser que tengamos millones de usuarios
@@ -54,9 +58,55 @@ class LoginController extends Controller
             }
             $try++;
         } while (User::where('remember_token', $token)->first() instanceof User);
-        //TODO Coger el $token, guardarlo en remember_token de users y mandar el email personalizado a no se quien (porque no existe ningun mail) usando el email predefinido de laravel para este caso de remember token o mejos aun, creando uno nuevo entero
 
-        dd($token);
+        $contact = User::where('contact', 'LIKE', '%'.$login.'%')->orwhere('dni', $login)->get();
+
+        /*foreach($contact as $i=>$ct) {
+           var_dump($contact[$i]->id);
+        }*/
+
+        User::where('id',$contact[0]->id)->update(['remember_token'=>$token]);
+        //TODO Coger el $token, guardarlo en remember_token de users y mandar el email personalizado a no se quien (porque no existe ningun mail) usando el email predefinido de laravel para este caso de remember token o mejor aun, creando uno nuevo entero
+
+        $passDbEnc = $user->password;
+        if(password_verify ($token, $passDbEnc)) {
+            dd(false);
+            if (Hash::needsRehash($passDbEnc)) {
+                dd(true);
+                $hashed = Hash::make($token);
+                $user->password = $hashed;
+                $user->save();
+            }
+        }
+
+        //$objContact = json_encode($contact);
+        $objContact = 'pedroramonmmspam1@gmail.com';
+        
+        Mail::send('login', ['user' => $objContact], function ($m) use ($user) {
+            $m->from('pedroramonmmspam1@gmail.com', 'Your Application');
+
+            $m->to('pedroramonmmspam1@gmail.com', 'Prueba')->subject('Your Reminder!');
+        });
+
+        /*
+mipagina.com/recovery/password/recuperar.php?cve=239483492384924829928934823948
+
+$_REQUEST["cve"]
+
+
+////
+Ha solicitado la recuperación de su contraseña
+
+Activiades pendientes:
+          1.-hacer un update del usuario por campo password(where debe ser unicamente por password)
+             -Crear un formulario con dos cajas de texto(input type text) a) Escribe contraseña nueva b) Confirma contraseña nueva , 3 input (hidden) tendra la clave recibida por REQUEST, button submit
+             -el controlador tendrà que hacer el update sobre el where password sea igual a la contraseña encriptada por el campo oculto
+             - una vez hecho el cambio, redireccionar al login para que se logue con su nueva contraseña
+
+Registro de paciente: formulario con el registro (crear modelo y controlador)
+
+
+        */
     }
 
     public function index() {
